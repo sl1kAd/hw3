20c1a3b7228d07b5f0d8a847e5c7f6119e834129
sl1kAd <80388890+sl1kAd@users.noreply.github.com>
2021-05-18 19:11:13 +0300

    Add files via upload

---
+++ ConsoleApplication8.cpp
@@ -0,0 +1,32 @@
+﻿#include <stdbool.h>
+#include <iostream>
+
+bool isPrime(int n)
+{
+  
+    if (n > 1)
+    {
+        for (int i = 2; i < n; i++)
+            if (n % i == 0) 
+                return false;
+
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+int main()
+{
+    int n;
+
+    printf("Input a number: ");
+
+    scanf_s("%d", &n);
+
+    printf("Is prime: %d\n", isPrime(n));
+
+    return 0;
+}
